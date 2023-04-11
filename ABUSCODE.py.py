51bcabccd3fcc3cfeea1c3d4d5730c4156b38c4f
Abubakar Saddique <127618239+Abu-Saddique@users.noreply.github.com>
2023-04-11 03:03:50 +0100

    Add files via upload

---
+++ ABUSCODE.py.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Multiply two numbers
+def multiply_numbers(n1,n2):
+    return n1*n2
+multiply_numbers(43,89)
+
+
+# In[2]:
+
+
+#Greet
+def greet(name):
+    print("Hello",name)
+    print("How do you do King?")
+greet("DKY CODE")
+
+
+# In[3]:
+
+
+#Calculate Average Height of Volley Ball players in a team 
+def average_height(n1,n2,n3,n4,n5,n6):
+    #Add height of players 
+    add= n1+n2+n3+n4+n5+n6
+    
+    #number of players
+    y=len([n1,n2,n3,n4,n5,n6])
+    
+    #average of height of players 
+    result=add/y
+    return result
+
+average_height(1,2,3,4,5,6)
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[4]:
+
+
+#Calculate average height of volleyball players
+def average_height(n1,n2,n3,n4,n5,n6):
+    return(n1+n2+n3+n4+n5+n6)/len([n1,n2,n3,n4,n5,n6])
+average_height(1,2,3,4,5,6)
+
+
+# In[19]:
+
+
+#Calculate area of a circle 
+def area_circle():
+    circle=22/7*(radius**2)
+    return cirlce
+area_circle=(7)
+
+
+# In[22]:
+
+
+#calculate the area of a cirlce 
+def area_circle(r):
+    circle=22/7*r**2
+    return circle 
+area_circle(7)
+
+
+# In[ ]:
+
+
+
+
